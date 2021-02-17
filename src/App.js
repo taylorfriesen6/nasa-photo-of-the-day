b77f980211837b49ba3fe97266ed48f37939b787
Taylor Friesen <taylorfriesen@protonmail.com>
2021-02-17 14:33:46 -0800

    handled network error

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
+
 import "./App.css";
 
 import InfoCard from "./InfoCard.js";
 
 function App() {
   const [data, setData] = useState({});
+  const [networkError, setNetworkError] = useState(false);
 
   useEffect(() => {
     Axios.get("https://api.nasa.gov/planetary/apod?api_key=7tMbzFwnoMa0dua93R3O4zh4M9ctDWp4vBEAikcQ")
-      .then(res => setData(res.data));
+      .then(res => setData(res.data))
+      .catch(_ => setNetworkError(true));
   }, []);
 
   console.log(data);
+
   return (
     <div className="App">
       <h1>NASA Photo of the Day</h1>
-      <InfoCard data={data}/>
+      {!networkError ? <InfoCard data={data}/> : <p>Sorry, the photo couldn't be retrieved :(</p>}
     </div>
   );
 }
